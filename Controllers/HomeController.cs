3c0825f80a00d3a2fd10343b982f96bf7c07e81f
Chayandip Das <chayandipdas@gmail.com>
2026-02-14 22:04:13 +0530

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace cicddemo.Controllers
 
         public IActionResult Index()
         {
-            ViewBag.Message = "Welcome to the CI/CD Demo Application! Chayandip das....";
+            ViewBag.Message = "Welcome to the CI/CD Demo Application! Chayandip das...";
             return View();
         }
 
